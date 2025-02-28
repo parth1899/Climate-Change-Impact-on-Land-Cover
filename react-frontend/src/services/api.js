76e00900822b253dc784b117bc951c18a14b77f1
Parth Kalani <parthkalani1899@gmail.com>
2025-02-28 15:22:16 +0530

    minor changes

--- react-frontend/src/services/api.js
+++ react-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || '/api';
+const API_BASE_URL = 'http://127.0.0.1:5000/api';
 
 export const fetchMapData = async (region, years, classes) => {
   try {
